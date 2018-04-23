1ee98111b2afcfabe53726ce34639718e85b0497
lmayorga <lmayorga@lm3corp.com>
2018-04-23 19:43:02 -0400

    (maint)add support for vmware_desktop plugin

---
+++ lib/beaker/hypervisor/vagrant_desktop.rb
@@ -0,0 +1,17 @@
+require 'beaker/hypervisor/vagrant'
+
+class Beaker::VagrantDesktop < Beaker::Vagrant
+  def provision(provider = 'vmware_desktop')
+    super
+  end
+
+  def self.provider_vfile_section(host, options)   
+    v_provider = "    v.vm.provider :vmware_desktop do |v|\n"
+    v_provider <<  "      v.vmx['memsize'] = '#{memsize(host,options)}'\n"
+    v_provider <<  "      v.vmx['whitelist_verified'] = '#{host['whitelist_verified']}'\n" unless host['whitelist_verified'].nil?
+    v_provider <<  "      v.vmx['functional_hgfs'] = '#{host['functional_hgfs']}'\n" unless host['functional_hgfs'].nil?
+    v_provider <<  "      v.vmx['unmount_default_hgfs'] = '#{host['unmount_default_hgfs']}'\n" unless host['unmount_default_hgfs'].nil?
+    v_provider <<  "      v.vmx['utility_port'] = '#{host['utility_port']}'\n" unless host['utility_port'].nil?
+    v_provider <<  "    end\n"
+  end
+end
---
+++ spec/beaker/hypervisor/vagrant_desktop_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe Beaker::VagrantDesktop do
+  let( :options ) { make_opts.merge({ :hosts_file => 'sample.cfg', 'logger' => double().as_null_object }) }
+  let( :vagrant ) { Beaker::VagrantDesktop.new( @hosts, options ) }
+
+  before :each do
+    @hosts = make_hosts()
+  end
+
+  it "uses the vmware_desktop provider for provisioning" do
+    @hosts.each do |host|
+      host_prev_name = host['user']
+      expect( vagrant ).to receive( :set_ssh_config ).with( host, 'vagrant' ).once
+      expect( vagrant ).to receive( :copy_ssh_to_root ).with( host, options ).once
+      expect( vagrant ).to receive( :set_ssh_config ).with( host, host_prev_name ).once
+    end
+    expect( vagrant ).to receive( :hack_etc_hosts ).with( @hosts, options ).once
+    expect( vagrant ).to receive( :vagrant_cmd ).with( "up --provider vmware_desktop" ).once
+    vagrant.provision
+  end
+
+  it "can make a Vagranfile for a set of hosts" do
+    path = vagrant.instance_variable_get( :@vagrant_path )
+    allow( vagrant ).to receive( :randmac ).and_return( "0123456789" )
+
+    vagrant.make_vfile( @hosts )
+
+    vagrantfile = File.read( File.expand_path( File.join( path, "Vagrantfile")))
+    expect( vagrantfile ).to include( %Q{    v.vm.provider :vmware_desktop do |v|\n      v.vmx['memsize'] = '1024'\n    end})
+  end
+
+  it "can enable whitelist_verified on hosts" do 
+    path = vagrant.instance_variable_get( :@vagrant_path )
+    hosts = make_hosts({:whitelist_verified => true},1)
+
+    vagrant.make_vfile( hosts )
+
+    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
+    expect( vagrantfile ).to include( %Q{ v.vmx['whitelist_verified'] = 'true'})
+  end
+
+  it "can enable functional_hgfs on hosts" do 
+    path = vagrant.instance_variable_get( :@vagrant_path )
+    hosts = make_hosts({:functional_hgfs => true},1)
+
+    vagrant.make_vfile( hosts )
+
+    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
+    expect( vagrantfile ).to include( %Q{ v.vmx['functional_hgfs'] = 'true'})
+  end
+
+  it "can enable unmount_default_hgfs on hosts" do 
+    path = vagrant.instance_variable_get( :@vagrant_path )
+    hosts = make_hosts({:unmount_default_hgfs => true},1)
+
+    vagrant.make_vfile( hosts )
+
+    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
+    expect( vagrantfile ).to include( %Q{ v.vmx['unmount_default_hgfs'] = 'true'})
+  end
+end
