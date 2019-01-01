caeaaf8211476c6ffbbbda5c1c1c25fa85f65408
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-01-01 20:54:46 +0100

    (MAINT) Fix windows tests

--- spec/beaker/hypervisor/vagrant_spec.rb
+++ spec/beaker/hypervisor/vagrant_spec.rb
@@ -636,10 +636,11 @@ EOF
       before :each do
         expect( vagrant ).to receive( :vagrant_cmd ).with( "up" ).once
         @hosts.each do |host|
+          host[:platform] = 'windows'
           host_prev_name = host['user']
-          expect( vagrant ).to receive( :set_ssh_config ).with( host, 'vagrant' ).once
-          expect( vagrant ).not_to receive( :copy_ssh_to_root ).with( host, options ).once
-          expect( vagrant ).not_to receive( :set_ssh_config ).with( host, host_prev_name ).once
+          expect( vagrant ).not_to receive( :set_ssh_config ).with( host, 'vagrant' )
+          expect( vagrant ).not_to receive( :copy_ssh_to_root ).with( host, options )
+          expect( vagrant ).to receive( :set_ssh_config ).with( host, host_prev_name ).once
         end
         expect( vagrant ).to receive( :hack_etc_hosts ).with( @hosts, options ).once
       end
