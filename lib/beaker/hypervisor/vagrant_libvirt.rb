voxpupuli/beaker-vagrant

commit b162e77b2f8c8f465a6267385c5a7d3f1a2d2bda

Fix simultaneous runs with the libvirt hypervisor
Previously, if any two test runs occurred where the systems had identical
hostnames, then one of the runs would fail indicating that there was a
domain conflict.

This patch ensures that a unique name is created for each run when using
the `libvirt` provider.

Additionally, the string `beaker_` is prepended to the path name so that
it is easier to find your systems when large numbers of VMs are running
at the same time.

It also chooses a different base network when using `libvirt` vs
anything else so that users can use both VirtualBox and `libvirt`
simultaneously.

lib/beaker/hypervisor/vagrant_libvirt.rb
@@ -16,6 +16,12 @@ class Beaker::VagrantLibvirt < Beaker::Vagrant
   end
 
   def provision(provider = 'libvirt')
+    # This needs to be unique for every system with the same hostname but does
+    # not affect VirtualBox
+    vagrant_path_digest = Digest::SHA256.hexdigest(@vagrant_path)
+    @vagrant_path = @vagrant_path + '_' + vagrant_path_digest[0..2] + vagrant_path_digest[-3..-1]
+    @vagrant_file = File.expand_path(File.join(@vagrant_path, "Vagrantfile"))
+
     super
   end
 

.rspec
@@ -1,3 +1,4 @@
 --format documentation
 --color
 --tty
+--fail-fast

lib/beaker/hypervisor/vagrant.rb
@@ -16,8 +16,13 @@ module Beaker
       (2 + rand(252)).to_s #don't want a 0, 1, or a 255
     end
 
-    def randip
-      "10.255.#{rand_chunk}.#{rand_chunk}"
+    def randip(hypervisor=nil)
+      case hypervisor
+      when /libvirt/
+        "10.254.#{rand_chunk}.#{rand_chunk}"
+      else
+        "10.255.#{rand_chunk}.#{rand_chunk}"
+      end
     end
 
     def private_network_generator(host)
@@ -57,7 +62,7 @@ module Beaker
 
       hosts.each do |host|
         host.name.tr!('_','-') # Rewrite Hostname with hyphens instead of underscores to get legal hostname
-        host['ip'] ||= randip #use the existing ip, otherwise default to a random ip
+        host['ip'] ||= randip(host.host_hash[:hypervisor]) #use the existing ip, otherwise default to a random ip
         v_file << "  c.vm.define '#{host.name}' do |v|\n"
         v_file << "    v.vm.hostname = '#{host.name}'\n"
         v_file << "    v.vm.box = '#{host['box']}'\n"
@@ -136,6 +141,9 @@ module Beaker
         @logger.debug "created Vagrantfile for VagrantHost #{host.name}"
       end
       v_file << "end\n"
+
+      # In case this is called directly
+      FileUtils.mkdir_p(@vagrant_path)
       File.open(@vagrant_file, 'w') do |f|
         f.write(v_file)
       end
@@ -232,8 +240,7 @@ module Beaker
       @logger = options[:logger]
       @temp_files = []
       @hosts = vagrant_hosts
-      @vagrant_path = File.expand_path(File.join(File.basename(__FILE__), '..', '.vagrant', 'beaker_vagrant_files', File.basename(options[:hosts_file])))
-      FileUtils.mkdir_p(@vagrant_path)
+      @vagrant_path = File.expand_path(File.join(File.basename(__FILE__), '..', '.vagrant', 'beaker_vagrant_files', 'beaker_' + File.basename(options[:hosts_file])))
       @vagrant_file = File.expand_path(File.join(@vagrant_path, "Vagrantfile"))
       @vagrant_env = { "RUBYLIB" => "", "RUBYOPT" => "" }
     end
@@ -254,6 +261,8 @@ module Beaker
     end
 
     def provision(provider = nil)
+      FileUtils.mkdir_p(@vagrant_path)
+
       #setting up new vagrant hosts
       #make sure that any old boxes are dead dead dead
       begin

spec/beaker/hypervisor/vagrant_spec.rb
@@ -29,11 +29,11 @@ module Beaker
       })
     end
 
-    it "stores the vagrant file in $WORKINGDIR/.vagrant/beaker_vagrant_files/sample.cfg" do
+    it "stores the vagrant file in $WORKINGDIR/.vagrant/beaker_vagrant_files/beaker_sample.cfg" do
       allow( vagrant ).to receive( :randmac ).and_return( "0123456789" )
       path = vagrant.instance_variable_get( :@vagrant_path )
 
-      expect( path ).to be === File.join(Dir.pwd, '.vagrant', 'beaker_vagrant_files', 'sample.cfg')
+      expect( path ).to be === File.join(Dir.pwd, '.vagrant', 'beaker_vagrant_files', 'beaker_sample.cfg')
 
     end
 

commit cf82b48262a9edf8c8e9b37ec512cd7a9b1afd08

Correctly override the Vagrant path
In b162e77b2f8c8f465a6267385c5a7d3f1a2d2bda the path was updated to make
the path unique. However, it's done in the provisioning step. That's the
wrong time. If BEAKER_provision=no is passed, no provisioning happens
and the path is incorrect. By overriding the initializer, it always
happens.

Fixes: b162e77b2f8c8f465a6267385c5a7d3f1a2d2bda

lib/beaker/hypervisor/vagrant_libvirt.rb
@@ -1,13 +1,17 @@
 require 'beaker/hypervisor/vagrant'
 
 class Beaker::VagrantLibvirt < Beaker::Vagrant
-  def provision(provider = 'libvirt')
+  def initialize(*)
+    super
+
     # This needs to be unique for every system with the same hostname but does
     # not affect VirtualBox
     vagrant_path_digest = Digest::SHA256.hexdigest(@vagrant_path)
-    @vagrant_path = @vagrant_path + '_' + vagrant_path_digest[0..2] + vagrant_path_digest[-3..-1]
+    @vagrant_path += '_' + vagrant_path_digest[0..2] + vagrant_path_digest[-3..-1]
     @vagrant_file = File.expand_path(File.join(@vagrant_path, "Vagrantfile"))
+  end
 
+  def provision(provider = 'libvirt')
     super
   end
 