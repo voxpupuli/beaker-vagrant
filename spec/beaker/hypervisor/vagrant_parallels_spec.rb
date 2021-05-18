fc7bcae21bec7f4e158397ff3406319e10c5dbc3
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-05-18 12:37:06 +0200

    Clean up spec files coding style
    This uses a let block for hosts. It also uses subject instead of
    @vagrantfile. While doing so, it also uses the variable @vagrant_file
    instead of @vagrant_path and then adding a static filename. This reduces
    the amount of logic required.

--- spec/beaker/hypervisor/vagrant_parallels_spec.rb
+++ spec/beaker/hypervisor/vagrant_parallels_spec.rb
@@ -2,42 +2,40 @@ require 'spec_helper'
 
 describe Beaker::VagrantParallels do
   let( :options ) { make_opts.merge({ :hosts_file => 'sample.cfg', 'logger' => double().as_null_object }) }
-  let( :vagrant ) { Beaker::VagrantParallels.new( @hosts, options ) }
-
-  before :each do
-    @hosts = make_hosts()
-  end
+  let( :vagrant ) { Beaker::VagrantParallels.new( hosts, options ) }
+  let( :hosts ) { make_hosts }
 
   it "uses the parallels provider for provisioning" do
-    @hosts.each do |host|
+    hosts.each do |host|
       host_prev_name = host['user']
       expect( vagrant ).to receive( :set_ssh_config ).with( host, 'vagrant' ).once
       expect( vagrant ).to receive( :copy_ssh_to_root ).with( host, options ).once
       expect( vagrant ).to receive( :set_ssh_config ).with( host, host_prev_name ).once
     end
-    expect( vagrant ).to receive( :hack_etc_hosts ).with( @hosts, options ).once
+    expect( vagrant ).to receive( :hack_etc_hosts ).with( hosts, options ).once
     expect( vagrant ).to receive( :vagrant_cmd ).with( "up --provider parallels" ).once
     vagrant.provision
   end
 
-  it "can make a Vagranfile for a set of hosts" do
-    path = vagrant.instance_variable_get( :@vagrant_path )
+  context 'Correct vagrant configuration' do
+    subject do
+      FakeFS do
+        vagrant.make_vfile( hosts, options )
+        File.read(vagrant.instance_variable_get(:@vagrant_file))
+      end
+    end
 
-    vagrant.make_vfile( @hosts )
-
-    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
-    expect( vagrantfile ).to include( %Q{    v.vm.provider :parallels do |prl|\n      prl.optimize_power_consumption = false\n      prl.memory = '1024'\n    end})
+    it "can make a Vagrantfile for a set of hosts" do
+      is_expected.to include( %Q{    v.vm.provider :parallels do |prl|\n      prl.optimize_power_consumption = false\n      prl.memory = '1024'\n    end})
+    end
   end
 
-  it "can disable the auto-update functionality of the Parallels Guest Tools" do
-    options.merge!({ :prl_update_guest_tools => 'disable' })
+  context 'disabled guest tools' do
+    let(:options) { super().merge({ :prl_update_guest_tools => 'disable' }) }
+    subject { vagrant.class.provider_vfile_section( hosts.first, options ) }
 
-    vfile_section = vagrant.class.provider_vfile_section( @hosts.first, options )
-
-    match = vfile_section.match(/prl.update_guest_tools = false/)
-
-    expect( match ).to_not be nil
-
+    it "can disable the auto-update functionality of the Parallels Guest Tools" do
+      is_expected.to match(/prl.update_guest_tools = false/)
+    end
   end
-
 end
--- spec/beaker/hypervisor/vagrant_virtualbox_spec.rb
+++ spec/beaker/hypervisor/vagrant_virtualbox_spec.rb
@@ -2,98 +2,77 @@ require 'spec_helper'
 
 describe Beaker::VagrantVirtualbox do
   let( :options ) { make_opts.merge({ :hosts_file => 'sample.cfg', 'logger' => double().as_null_object }) }
-  let( :vagrant ) { Beaker::VagrantVirtualbox.new( @hosts, options ) }
-  let(:vagrantfile_path) do
-    path = vagrant.instance_variable_get( :@vagrant_path )
-    File.expand_path( File.join( path, 'Vagrantfile' ))
-  end
-
-  before :each do
-    @hosts = make_hosts()
-  end
+  let( :vagrant ) { described_class.new( hosts, options ) }
+  let(:vagrantfile_path) { vagrant.instance_variable_get( :@vagrant_file ) }
+  let(:hosts) { make_hosts() }
 
   it "uses the virtualbox provider for provisioning" do
-    @hosts.each do |host|
+    hosts.each do |host|
       host_prev_name = host['user']
       expect( vagrant ).to receive( :set_ssh_config ).with( host, 'vagrant' ).once
       expect( vagrant ).to receive( :copy_ssh_to_root ).with( host, options ).once
       expect( vagrant ).to receive( :set_ssh_config ).with( host, host_prev_name ).once
     end
-    expect( vagrant ).to receive( :hack_etc_hosts ).with( @hosts, options ).once
+    expect( vagrant ).to receive( :hack_etc_hosts ).with( hosts, options ).once
     expect( vagrant ).to receive( :vagrant_cmd ).with( "up --provider virtualbox" ).once
     vagrant.provision
   end
 
-  it "can make a Vagranfile for a set of hosts" do
-    path = vagrant.instance_variable_get( :@vagrant_path )
+  context 'can make a Vagrantfile' do
+    subject do
+      FakeFS do
+        vagrant.make_vfile(hosts)
+        File.read(vagrant.instance_variable_get(:@vagrant_file))
+      end
+    end
 
-    vagrant.make_vfile( @hosts )
+    it "can make a Vagrantfile for a set of hosts" do
+      is_expected.to include( %Q{    v.vm.provider :virtualbox do |vb|\n      vb.customize ['modifyvm', :id, '--memory', '1024', '--cpus', '1', '--audio', 'none']\n    end})
+    end
 
-    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
-    expect( vagrantfile ).to include( %Q{    v.vm.provider :virtualbox do |vb|\n      vb.customize ['modifyvm', :id, '--memory', '1024', '--cpus', '1', '--audio', 'none']\n    end})
+    context 'with ioapic(multiple cores)' do
+      let(:hosts) { make_hosts({:ioapic => 'true'}, 1) }
+
+      it { is_expected.to include( " vb.customize ['modifyvm', :id, '--ioapic', 'on']") }
+    end
+
+    context 'with NAT DNS' do
+      let(:hosts) { make_hosts({:natdns => 'on'}, 1) }
+
+      it { is_expected.to include( " vb.customize ['modifyvm', :id, '--natdnshostresolver1', 'on']") }
+      it { is_expected.to include( " vb.customize ['modifyvm', :id, '--natdnsproxy1', 'on']") }
+    end
+
+    context 'storage with the USB controller' do
+      let(:hosts) { make_hosts({:volumes => { 'test_disk' => { size: '5120' }}, :volume_storage_controller => 'USB' }) }
+
+      it { is_expected.to include(" vb.customize ['modifyvm', :id, '--usb', 'on']") }
+      it { is_expected.to include(" vb.customize ['storagectl', :id, '--name', 'Beaker USB Controller', '--add', 'usb', '--portcount', '8', '--controller', 'USB', '--bootable', 'off']") }
+      it { is_expected.to include(" vb.customize ['createhd', '--filename', 'vm1-test_disk.vdi', '--size', '5120']") }
+      it { is_expected.to include(" vb.customize ['storageattach', :id, '--storagectl', 'Beaker USB Controller', '--port', '0', '--device', '0', '--type', 'hdd', '--medium', 'vm1-test_disk.vdi']") }
+    end
+
+    context 'storage with the LSILogic controller' do
+      let(:hosts) { make_hosts({:volumes => { 'test_disk' => { size: '5120' }}, :volume_storage_controller => 'LSILogic' }) }
+
+      it { is_expected.to include(" vb.customize ['storagectl', :id, '--name', 'Beaker LSILogic Controller', '--add', 'scsi', '--portcount', '16', '--controller', 'LSILogic', '--bootable', 'off']") }
+      it { is_expected.to include(" vb.customize ['createhd', '--filename', 'vm1-test_disk.vdi', '--size', '5120']") }
+      it { is_expected.to include(" vb.customize ['storageattach', :id, '--storagectl', 'Beaker LSILogic Controller', '--port', '0', '--device', '0', '--type', 'hdd', '--medium', 'vm1-test_disk.vdi']") }
+    end
+
+    context "storage with the default controller" do
+      let(:hosts) { make_hosts({:volumes => { 'test_disk' => { size: '5120' }}}) }
+
+      it { is_expected.to include(" vb.customize ['storagectl', :id, '--name', 'Beaker IntelAHCI Controller', '--add', 'sata', '--portcount', '2', '--controller', 'IntelAHCI', '--bootable', 'off']") }
+      it { is_expected.to include(" vb.customize ['createhd', '--filename', 'vm1-test_disk.vdi', '--size', '5120']") }
+      it { is_expected.to include(" vb.customize ['storageattach', :id, '--storagectl', 'Beaker IntelAHCI Controller', '--port', '0', '--device', '0', '--type', 'hdd', '--medium', 'vm1-test_disk.vdi']") }
+    end
   end
 
-  it "can disable the vb guest plugin" do
-    options.merge!({ :vbguest_plugin => 'disable' })
+  context 'disabled vb guest plugin' do
+    let(:options) { super().merge({ :vbguest_plugin => 'disable' }) }
+    subject { vagrant.class.provider_vfile_section( hosts.first, options ) }
 
-    vfile_section = vagrant.class.provider_vfile_section( @hosts.first, options )
-
-    match = vfile_section.match(/vb.vbguest.auto_update = false/)
-
-    expect( match ).to_not be nil
-
-  end
-
-  it "can enable ioapic(multiple cores) on hosts" do
-    path = vagrant.instance_variable_get( :@vagrant_path )
-    hosts = make_hosts({:ioapic => 'true'},1)
-
-    vagrant.make_vfile( hosts )
-
-    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
-    expect( vagrantfile ).to include( " vb.customize ['modifyvm', :id, '--ioapic', 'on']")
-  end
-
-  it "can enable NAT DNS on hosts" do
-    hosts = make_hosts({:natdns => 'on'},1)
-    vagrant.make_vfile( hosts )
-    vagrantfile = File.read( vagrantfile_path )
-
-    expect( vagrantfile ).to include( " vb.customize ['modifyvm', :id, '--natdnshostresolver1', 'on']")
-    expect( vagrantfile ).to include( " vb.customize ['modifyvm', :id, '--natdnsproxy1', 'on']")
-  end
-
-  it "correctly provisions storage with the USB controller" do
-    path = vagrant.instance_variable_get( :@vagrant_path )
-    hosts = make_hosts({:volumes => { 'test_disk' => { size: '5120' }}, :volume_storage_controller => 'USB' })
-
-    vagrant.make_vfile( hosts )
-    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
-    expect( vagrantfile ).to include(" vb.customize ['modifyvm', :id, '--usb', 'on']")
-    expect( vagrantfile ).to include(" vb.customize ['storagectl', :id, '--name', 'Beaker USB Controller', '--add', 'usb', '--portcount', '8', '--controller', 'USB', '--bootable', 'off']")
-    expect( vagrantfile ).to include(" vb.customize ['createhd', '--filename', 'vm1-test_disk.vdi', '--size', '5120']")
-    expect( vagrantfile ).to include(" vb.customize ['storageattach', :id, '--storagectl', 'Beaker USB Controller', '--port', '0', '--device', '0', '--type', 'hdd', '--medium', 'vm1-test_disk.vdi']")
-  end
-
-  it "correctly provisions storage with the LSILogic controller" do
-    path = vagrant.instance_variable_get( :@vagrant_path )
-    hosts = make_hosts({:volumes => { 'test_disk' => { size: '5120' }}, :volume_storage_controller => 'LSILogic' })
-
-    vagrant.make_vfile( hosts )
-    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
-    expect( vagrantfile ).to include(" vb.customize ['storagectl', :id, '--name', 'Beaker LSILogic Controller', '--add', 'scsi', '--portcount', '16', '--controller', 'LSILogic', '--bootable', 'off']")
-    expect( vagrantfile ).to include(" vb.customize ['createhd', '--filename', 'vm1-test_disk.vdi', '--size', '5120']")
-    expect( vagrantfile ).to include(" vb.customize ['storageattach', :id, '--storagectl', 'Beaker LSILogic Controller', '--port', '0', '--device', '0', '--type', 'hdd', '--medium', 'vm1-test_disk.vdi']")
-  end
-
-  it "correctly provisions storage with the default controller" do
-    path = vagrant.instance_variable_get( :@vagrant_path )
-    hosts = make_hosts({:volumes => { 'test_disk' => { size: '5120' }}})
-
-    vagrant.make_vfile( hosts )
-    vagrantfile = File.read( File.expand_path( File.join( path, 'Vagrantfile' )))
-    expect( vagrantfile ).to include(" vb.customize ['storagectl', :id, '--name', 'Beaker IntelAHCI Controller', '--add', 'sata', '--portcount', '2', '--controller', 'IntelAHCI', '--bootable', 'off']")
-    expect( vagrantfile ).to include(" vb.customize ['createhd', '--filename', 'vm1-test_disk.vdi', '--size', '5120']")
-    expect( vagrantfile ).to include(" vb.customize ['storageattach', :id, '--storagectl', 'Beaker IntelAHCI Controller', '--port', '0', '--device', '0', '--type', 'hdd', '--medium', 'vm1-test_disk.vdi']")
+    it { is_expected.to match(/vb\.vbguest\.auto_update = false/) }
   end
 end
